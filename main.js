20bf290978392eb3d5861e9e1b40114500be1450
vanessa <NovaDReaper@yahoo.com>
2023-10-26 15:48:35 -0400

    removed old coments no longer needed

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// 1. Create customer Divs with borders
-// 2. Create custoner name divs with borders
-// 3. Get customer names to show on the webpage
-
 const dataBase = document.querySelector(".container");
 
 function customerCard(customer) {
@@ -34,27 +30,6 @@ function customerCard(customer) {
           </ul>
         </div>`;
 }
-
-/*
-for (let customer of customers) {
-  //--test to make sure for loop working---
-  console.log(customer);
-
-  //-------making contairer for all the data of each person----------
-  let custContain = document.createElement("div");
-  custContain.classList.add("customer");
-  //---------base for each section needed?-------------
-  let custName = document.createElement("div");
-  custName.classList.add("name");
-  custName.innerText = `${customer.name.first} ${customer.name.last}`;
-  custContain.appendChild(custName);
-  dataBase.appendChild(custContain);
-}
-
-function customerCard(customers) {
-  return console.log(customer);
-}
-*/
 
 let dataBaseFilled = customers
   .map((customer) => customerCard(customer))
